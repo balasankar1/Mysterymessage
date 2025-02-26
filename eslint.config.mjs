7a393fbec7f87565a5bcadac967c4e3093e87782
balasankar1 <balasankar0143@gmail.com>
2025-02-26 06:45:54 +0530

    initial setup complete

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,6 +8,11 @@ const __dirname = dirname(__filename);
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
+module.exports = {
+  rules: {
+    "@typescript-eslint/no-unused-vars": "off",
+  },
+};
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
